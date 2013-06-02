ac6a0da44b0653cfd7386f859162352846aa9aae
Antonio Ospite <ao2@amarulasolutions.com>
2013-06-03 00:13:08 +0200

    hiface_pcm_close(): close a race window
    Stop the USB stream before deactivating it.
    
    Signed-off-by: Antonio Ospite <ao2@amarulasolutions.com>

--- pcm.c
+++ pcm.c
@@ -406,15 +406,16 @@ static int hiface_pcm_close(struct snd_pcm_substream *alsa_sub)
 
 	mutex_lock(&rt->stream_mutex);
 	if (sub) {
+		/* all substreams closed? if so, stop streaming */
+		if (!rt->playback.instance)
+			hiface_pcm_stream_stop(rt);
+
 		/* deactivate substream */
 		spin_lock_irqsave(&sub->lock, flags);
 		sub->instance = NULL;
 		sub->active = false;
 		spin_unlock_irqrestore(&sub->lock, flags);
 
-		/* all substreams closed? if so, stop streaming */
-		if (!rt->playback.instance)
-			hiface_pcm_stream_stop(rt);
 	}
 	mutex_unlock(&rt->stream_mutex);
 	return 0;
